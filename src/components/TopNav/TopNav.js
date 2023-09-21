01eb664ca2eacf34dab4f7b507406e8a5fe16cdd
marse01 <gmlqls4732@naver.com>
2023-09-21 15:33:55 +0900

    home추가

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -14,23 +14,38 @@ const TopNav = () => {
       alert('현재 사이트 링크가 클립보드에 복사되었습니다!');
     };
 
-  return (
-    <div className={styles.container}>
-      {/* 로고 */}
-      <img src={Logo} alt="logo" />
-      <div className={styles.rightButtonContainer}>
-        {/*앱 다운로드 버튼 */}
-        <a href={APP_DOWNLOAD_LINK} target="_blank">
-          <div className={styles.roundButton}>앱 다운로드</div>
-        </a>
-
-        {/* 공유 버튼 */}
-        <div className={styles.circleButton} onClick={onClickCopyButton}>
-          <img src={shareIcon} alt="share" />
+    return (
+      <div className={styles.container}>
+        <div className={styles.title_container}>
+             {/* 로고 */}
+          <img src={Logo} alt="logo" />
+          <div className={styles.rightButtonContainer}>
+            {/*앱 다운로드 버튼 */}
+            <a href={APP_DOWNLOAD_LINK} target="_blank">
+              <div className={styles.roundButton}>앱 다운로드</div>
+            </a>
+    
+            {/* 공유 버튼 */}
+            <div className={styles.circleButton} onClick={onClickCopyButton}>
+              <img src={shareIcon} alt="share" />
+            </div>
+          </div>
         </div>
+
+
+
+        <div className={styles.nav_container}>
+          <div className={styles.sub_nav}> 
+            국내주식
+            </div>
+          <div className={styles.sub_nav}>
+            해외주식
+            </div>
+        </div>
+     
       </div>
-    </div>
-  );
-};
+
+    );
+  };
 
 export default TopNav;
