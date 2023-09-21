9c6a8e5196a2282d3f8d4e691ba3a9283307c3ed
marse01 <gmlqls4732@naver.com>
2023-09-21 10:54:51 +0900

    상단바 만들기

---
+++ src/components/TopNav/TopNav.js
@@ -0,0 +1,36 @@
+import styles from "./TopNav.module.css";
+import Logo from "../../assets/images/logo.svg";
+import shareIcon from "../../assets/images/ic_share.svg";
+
+const TopNav = () => {
+  const APP_DOWNLOAD_LINK =
+    "https://open.shinhansec.com/phone/goM.jsp?p=OTUxMCYmJiZTJiY*&amp;v=2";
+
+    const onClickCopyButton = () => {
+      const siteLink = window.location.href; // 현재 사이트의 링크를 가져옵니다.
+      navigator.clipboard.writeText(siteLink); // 클립보드에 복사합니다.
+  
+      // 알림창을 띄웁니다.
+      alert('현재 사이트 링크가 클립보드에 복사되었습니다!');
+    };
+
+  return (
+    <div className={styles.container}>
+      {/* 로고 */}
+      <img src={Logo} alt="logo" />
+      <div className={styles.rightButtonContainer}>
+        {/*앱 다운로드 버튼 */}
+        <a href={APP_DOWNLOAD_LINK} target="_blank">
+          <div className={styles.roundButton}>앱 다운로드</div>
+        </a>
+
+        {/* 공유 버튼 */}
+        <div className={styles.circleButton} onClick={onClickCopyButton}>
+          <img src={shareIcon} alt="share" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopNav;
